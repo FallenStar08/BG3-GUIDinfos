22225124bba50105200c5b82eaaa4241e796001b
FallenStar <lasalle.joffrey@gmail.com>
2023-12-05 02:43:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# <Your Extension Name>
+
+[![Visual Studio Marketplace Version](https://img.shields.io/visual-studio-marketplace/v/your-extension-publisher.your-extension-name.svg)](https://marketplace.visualstudio.com/items?itemName=your-extension-publisher.your-extension-name)
+[![License](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/your-github-username/your-extension-repo/blob/main/LICENSE)
+
+## Overview
+
+Provides information about hovered tag GUIDs. It identifies GUIDs in your code and displays their associated name and description using data from a provided JSON file.
+
+## Features
+
+- **Hover Information:** Hover over a GUID in your code to see detailed information about the associated tag.
+
+## Installation
+
+1. Open Visual Studio Code.
+2. Go to the Extensions view by clicking on the Extensions icon in the Activity Bar on the side of the window or use the keyboard shortcut `Ctrl+Shift+X`.
+3. Search for "<Your Extension Name>".
+4. Click the "Install" button.
+
+## Usage
+
+1. Open a file in Visual Studio Code containing tag GUIDs.
+2. Hover over a tag GUID in the code.
+3. The extension will display information about the tag, including its name and description.
+
+## Configuration
+
+No additional configuration is required for this extension.
+
+## Extension Settings
+
+There are no customizable settings for this extension.
+
+## Known Issues
+
+- No known issues.
+
+## Contributing
+
+Contributions are welcome! If you encounter any issues or have suggestions, please feel free to [create an issue](https://github.com/your-github-username/your-extension-repo/issues) or submit a pull request.
+
+## License
+
+This extension is [MIT licensed](LICENSE).
+
+## Contact
+
+For questions or feedback, you can reach out to the author, FallenStar, on [GitHub](https://github.com/FallenStar).
+
+## Release Notes
+
+- Version X.X.X: Initial release.
+- See [CHANGELOG.md](CHANGELOG.md) for detailed release notes.
+
+## Acknowledgments
+
+Special thanks to [Visual Studio Code](https://code.visualstudio.com/) and the open-source community for their support.
+
---
+++ extension.js
@@ -0,0 +1,82 @@
+const vscode = require('vscode');
+const path = require('path');
+const fs = require('fs');
+
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+    // it's a thingy that does things on hover, yes. We register it.
+    let hoverProvider = vscode.languages.registerHoverProvider('*', {
+        provideHover(document, position, token) {
+            // Get the entire line at the current position
+            const line = document.lineAt(position.line).text;
+
+            // Cringe regex
+            const guidMatch = line.match(/[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}/);
+
+            if (guidMatch) {
+                const guid = guidMatch[0];
+
+                //console.log('Hovered GUID:', guid);  // Log the GUID
+
+                if (guid) {
+                    const tagInfo = lookupTagInfoForGUID(guid);
+                    //console.log('Tag Info for GUID:', tagInfo);  // Log the tag info
+
+                    if (tagInfo) {
+                        const name = tagInfo.Name;
+                        const description = tagInfo.Description;
+
+                        let hoverText = `**Name :** ${name}`;
+                        if (description) {
+                            hoverText += `\n\n**Description:** ${description}`;
+                        }
+
+                        return new vscode.Hover(hoverText);
+                    }
+                }
+            }
+
+            return null;
+        }
+    });
+
+    context.subscriptions.push(hoverProvider);
+}
+
+function lookupTagInfoForGUID(guid) {
+    const tagsFilePath = path.join(__dirname, 'AllTags.json');
+
+    try {
+        const tagsData = JSON.parse(fs.readFileSync(tagsFilePath, 'utf8'));
+
+        // Search for the GUID in the tags data...
+        for (const tagName in tagsData) {
+            if (tagsData.hasOwnProperty(tagName)) {
+                const tagInfo = tagsData[tagName];
+                if (tagInfo.UUID === guid) {
+                    // Return the tag information corresponding to the GUID
+                    return {
+                        Name: tagName,
+                        Description: tagInfo.Description
+                    };
+                }
+            }
+        }
+
+        return null;
+    } catch (error) {
+        console.error('Error reading tags file:', error);
+        return null;
+    }
+}
+
+// Idk wtf this is but guide had it
+function deactivate() {}
+
+module.exports = {
+    activate,
+    deactivate
+};
